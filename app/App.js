3e127162ceb0133fb9743f18542b7df3141ee2d1
Bruno Fidelis <bruno.fidelis004@gmail.com>
2018-05-01 16:19:09 -0300

    criando a estrutura do server

--- app/App.js
+++ app/App.js
@@ -1,13 +1,12 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
-export default class App extends React.Component {
+export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
+        <Text>Oi!</Text>
+        <Text>teste.</Text>
       </View>
     );
   }
@@ -16,6 +15,7 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    color: 'red',
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
