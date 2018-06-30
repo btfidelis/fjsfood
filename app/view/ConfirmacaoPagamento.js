880edad32281ce0f1cbd7e2ba0630788c62a239a
Ana Jornada <you@example.com>
2018-06-30 14:29:29 -0300

    Tela de Confirmação de Pagamento

--- app/view/ConfirmacaoPagamento.js
+++ app/view/ConfirmacaoPagamento.js
@@ -2,9 +2,39 @@ import React, { Component } from 'react';
 import { 
     View,
     Image, 
+    StyleSheet,
+    Text,
+    Button,
 } from 'react-native';
 
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: 30,
+        alignItems: 'center',
+        height: '20%',
+        justifyContent: 'space-between',
 
+        
+    },
+
+    localView: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        padding: 20,
+    },
+
+    baseText:{
+        fontFamily: 'Roboto'
+    },
+
+    buttonStyle: {
+  
+        alignItems: 'center',
+       
+        //marginTop: 50,
+    }
+})
 
 export default class ConfirmacaoPagamento extends Component { //app nome do arquivo
     
@@ -18,14 +48,26 @@ export default class ConfirmacaoPagamento extends Component { //app nome do arqu
 
      render(){
         return(
-            <View>
+            <View style={styles.localView}>
 
-                <Image 
-                    source = {{ uri: 'http://zanottirefrigeracao.com.br/blog/wp-content/uploads/segmento-restaurante-ecomanda-705x296.jpg' }} 
-                    style = {{width: 100, height: 70, marginTop: 20}} 
+                <Image style={styles.container}
+                    source = {{ uri: 'https://cdn.icon-icons.com/icons2/1380/PNG/512/vcsnormal_93488.png' }} 
+                    style = {{width: 200, height: 200, marginTop: 20,}} 
                 />
-                
+
+                <Text style={{marginBottom:60}}>
+                    Seu pagamento foi aprovado!
+                </Text>
+
+                <Button style={styles.buttonStyle}
+                        onPress={() => this.props.navigation.navigate('Carrinho')}// Trocar navegação para tela de dados do pagamento
+                        title="   Avalie seu pedido   "
+                        color="#FFC107"
+                        accessibilityLabel="Learn more about this purple button"
+                />
             </View>
+
+            
             );
         }
 
